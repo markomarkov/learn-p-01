e92f47c83bbf09d7391bc2bd2bdb457dca8c7eab
Marko Markov <stormshadow973@gmail.com>
2024-07-06 13:50:15 +0300

    chore: Update npm dependency to latest stable version

--- apps/learning-01/webpack.config.js
+++ apps/learning-01/webpack.config.js
@@ -1,21 +1,33 @@
+// const { NxWebpackPlugin } = require('@nx/webpack');
+// const { join } = require('path');
 
-const { NxWebpackPlugin } = require('@nx/webpack');
+// module.exports = {
+//   output: {
+//     path: join(__dirname, '../../dist/apps/learning-01'),
+//   },
+//   plugins: [
+//     new NxWebpackPlugin({
+//       target: 'node',
+//       // compiler: 'tsc',
+//       // main: './src/main.ts',
+//       // tsConfig: './tsconfig.app.json',
+//       // assets: ["./src/assets"],
+//       // optimization: false,
+//       // outputHashing: 'none',
+//     })
+//   ],
+// };
+
+const { compilePlugins, withNx, composePlugins } = require('@nx/webpack');
 const { join } = require('path');
 
-module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/learning-01'),
-  },
-  plugins: [
-    new NxWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ["./src/assets"],
-      optimization: false,
-      outputHashing: 'none',
-    })
-  ],
-};
-
+module.exports = composePlugins(
+  withNx({
+    target: 'node',
+    generatePackageJson: true,
+  }),
+  (config) => {
+    console.log(config);
+    return config;
+  }
+);
